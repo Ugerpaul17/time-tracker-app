6dd3868b79310578c678bf9188bf5d69ccf9f067
ugerpaul17 <ugerpaul5@gmail.com>
2021-02-10 18:33:57 +0300

    add style to login

--- components/Login.js
+++ components/Login.js
@@ -7,28 +7,32 @@ function Login() {
       <div className={styles.title}>
         <h2>Sign In</h2>
       </div>
-      <form className={styles.form}>
-        <input
-          id="email"
-          name="email"
-          type="text"
-          placeholder="Email"
-          autoComplete="email"
-          required
-          className={styles.inputBoxEmail}
-        />
-        <input
-          id="password"
-          name="password"
-          placeholder="Password"
-          type="password"
-          required
-          className={styles.inputBoxEmail}
-        />
-        <button type="submit" className={styles.but}>
-          Login
-        </button>
-      </form>
+      <div>
+        <form className={styles.form}>
+          <input
+            id="email"
+            name="email"
+            type="text"
+            placeholder="Email"
+            autoComplete="email"
+            required
+            className={styles.inputBoxEmail}
+          />
+          <input
+            id="password"
+            name="password"
+            placeholder="Password"
+            type="password"
+            required
+            className={styles.inputBoxEmail}
+          />
+          <div>
+            <button type="submit" className={styles.but}>
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
