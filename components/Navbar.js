e8ed9c588eb4919f960647365b55d5094f104ca8
ugerpaul17 <ugerpaul5@gmail.com>
2021-02-11 17:18:42 +0300

    create a new file Navbar and create navbar with links

---
+++ components/Navbar.js
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+
+
+function Navbar() {
+      
+
+    return (
+     <nav className="navbar navbar-expand-lg bg-primary navbar-dark bg-dark mb-4">
+      <a className="navbar-brand" href="#">
+        Clocking
+      </a>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link href="/">
+              <a className="nav-link">Home</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/report">
+              <a className="nav-link">Report</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/user">
+              <a className="nav-link">User</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/logout">
+              <a className="nav-link">Log Out</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    );
+}
+
+export default Navbar
