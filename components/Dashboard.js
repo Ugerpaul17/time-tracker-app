b3035e0f5667133a7abfc92d5d289396a1640ab5
ugerpaul17 <ugerpaul5@gmail.com>
2021-02-11 17:20:02 +0300

    create a new file dashboard

---
+++ components/Dashboard.js
@@ -0,0 +1,20 @@
+import Layout from './Layout';
+
+function Dashboard() {
+
+
+    return (
+        <div>
+            <Layout>
+    
+            </Layout>
+        </div>
+    )
+    
+}
+
+
+
+
+
+export default Dashboard
