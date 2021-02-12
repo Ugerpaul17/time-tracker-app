685b9ae877e5289c7cdb540bb2570c78f7250852
ugerpaul17 <ugerpaul5@gmail.com>
2021-02-12 11:20:18 +0300

    Add date and time function

---
+++ components/DateTime.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+
+export const DateTime = () => {
+    
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => setDate(new Date()), 1000)
+        return function cleanup() {
+            cleanup(timer)
+        }
+    },[]);
+
+    return ( 
+        <div>
+            <p>Time : {date.toLocaleTimeString()}</p>
+            <p>Date : {date.toLocaleDateString()}</p>
+        </div>
+        
+    );
+}
+
+export default DateTime
